867dd41f76e422422082a3d7ec186ce51f3639f1
faizahmad-khan <amanfaiz92@gmail.com>
2026-04-12 14:11:42 +0530

    Add initial admin configuration for Appointment and Prescription models in appointments app

---
+++ healthcare_dashboard/appointments/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Appointment, Prescription, PrescriptionItem
+admin.site.register(Appointment)
+admin.site.register(Prescription)
+admin.site.register(PrescriptionItem)
