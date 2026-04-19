b003044c17be6c6343cf46f0ea3d415797643d90
faizahmad-khan <amanfaiz92@gmail.com>
2026-04-19 23:16:56 +0530

    feat(admin): register pharmacy models in admin interface

---
+++ healthcare_dashboard/pharmacy/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Medicine, MedicineCategory, Dispensing, StockMovement
+admin.site.register(Medicine)
+admin.site.register(MedicineCategory)
+admin.site.register(Dispensing)
+admin.site.register(StockMovement)
